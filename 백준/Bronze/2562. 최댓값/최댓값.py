69486447c1716ed356ab9bf81d47da29c36bccfa
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-07 17:12:09 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+arr = [a, b, c, d, e, f, g, h, i]
+print(max(arr),  arr.index(max(arr))+1, sep="\n")
