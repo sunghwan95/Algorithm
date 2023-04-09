ace4ebfd02635fa413d6b1048782608cd2f8ecd1
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-10 04:41:31 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+arr = []
+
+
+def dfs():
+    if len(arr) == M:
+        print(" ".join(map(str, arr)))
+        return
+
+    for i in range(1, N+1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+
+dfs()
