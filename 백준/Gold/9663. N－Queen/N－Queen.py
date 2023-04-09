1f95818c587d2566bfd7bb658104e1731a180159
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-09 20:27:15 +0900

    [Gold IV] Title: N-Queen, Time: 29812 ms, Memory: 204896 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,6 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+row = [0]*N
+count = 0
+
+
 def check(x):
     for i in range(x):
-        if (col[x] == col[i]) or (abs(col[x]-col[i]) == x-i):
+        # 같은 열에 퀸이 존재하거나 대각선에 퀸이 있는 경우 False 반환.
+        if (row[x] == row[i]) or (abs(row[x]-row[i]) == abs(x-i)):
             return False
     return True
 
@@ -12,14 +20,12 @@ def recursion(x):
         count += 1
     else:
         for i in range(N):
-            col[x] = i
+            # (x,i)좌표에 퀸을 놓는다.
+            row[x] = i
+            # 퀸을 해당위치에 넣을 수 있는지 없는지 확인
             if check(x):
                 recursion(x+1)
 
-
-N = int(input())
-col = [0]*N
-count = 0
 
 recursion(0)
 print(count)
