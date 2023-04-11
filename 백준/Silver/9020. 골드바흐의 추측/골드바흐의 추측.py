53d0c4535a4f2a9d1b86c7338ccfcf44115dd86d
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-12 07:10:02 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 204 ms, Memory: 116364 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,16 +1,24 @@
 import sys
-input = sys.stdin.readline
-def isPrime(n):
-    if n == 1:
-        return 0
-    else:
-        for i in range(2, int(n**0.5)+1):
-            if n % i == 0:
-                return 0
-        return 1
-for _ in range(int(input())):
-    n = int(input())
-    for a in range(n//2,1,-1):
-        if isPrime(a) and isPrime(n-a):
-            print(a,n-a)
+T = int(sys.stdin.readline())
+arr = []
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    arr.append(n)
+
+
+def isPrime(partition):
+    for x in range(2, int(partition**0.5)+1):
+        if partition % x == 0:
+            return False
+    return True
+
+
+for num in arr:
+    partition1, partition2 = num//2, num//2
+    while partition1 > 0:
+        if isPrime(partition1) and isPrime(partition2):
+            print(partition1, partition2)
             break
+        else:
+            partition1 -= 1
+            partition2 += 1
