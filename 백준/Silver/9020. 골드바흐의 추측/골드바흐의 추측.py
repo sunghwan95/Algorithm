d948a597e5929a33807ca96afa49c1658f398919
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-12 00:57:02 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 220 ms, Memory: 116940 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,24 +1,16 @@
-import sys, math, queue
-
-prime_list = []
-
-for i in range(2, 10001):
-    isPrime = True
-    for prime in prime_list:
-        if i % prime == 0:
-            isPrime = False
-            break
-    if isPrime:
-        prime_list.append(i)
-
-t = int(sys.stdin.readline())
-
-for _ in range(t):
-    n = int(sys.stdin.readline())
-
-    for i in range(0, n//2):
-        a = n//2 - i
-        b = n//2 + i
-        if a in prime_list and b in prime_list:
-            print(str(a) + ' ' + str(b))
+import sys
+input = sys.stdin.readline
+def isPrime(n):
+    if n == 1:
+        return 0
+    else:
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                return 0
+        return 1
+for _ in range(int(input())):
+    n = int(input())
+    for a in range(n//2,1,-1):
+        if isPrime(a) and isPrime(n-a):
+            print(a,n-a)
             break
