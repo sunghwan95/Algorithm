0b73adb94394a0aef23a9417f3ab95efc5996eae
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-12 08:58:24 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,10 +1,27 @@
 N, r, c = map(int, input().split())
 
-
-def recursion(N, r, c):
-    if N == 0:
-        return 0
-    return 4*recursion(N-1, r//2, c//2)+2*(r % 2)+c % 2
+answer = 0
 
 
-print(recursion(N, r, c))
+def recursion(N, x, y):
+    global answer
+
+    if x == r and y == c:
+        print(int(answer))
+        return
+
+    if N == 1:
+        answer += 1
+        return
+
+    if not ((x <= r < x+N) and (y <= c < y+N)):
+        answer += N*N
+        return
+
+    recursion(N/2, x, y)
+    recursion(N/2, x, y+N/2)
+    recursion(N/2, x+N/2, y)
+    recursion(N/2, x+N/2, y+N/2)
+
+
+recursion(2**N, 0, 0)
