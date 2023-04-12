c657d87e4a6e5eb24c170d579f9694643f9f3ecd
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-12 11:30:47 +0900

    Update Z.py

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -3,25 +3,32 @@ N, r, c = map(int, input().split())
 answer = 0
 
 
-def recursion(N, x, y):
+def recursion(length, x, y):
     global answer
 
+    # 찾는 행과 열에 도달했을 때
     if x == r and y == c:
         print(int(answer))
         return
-
-    if N == 1:
+    
+    # 더 쪼갤 수 없을만큼 쪼갰을때 하나씩 탐색하기(1씩 더하기.)
+    if length == 1:
         answer += 1
         return
-
-    if not ((x <= r < x+N) and (y <= c < y+N)):
-        answer += N*N
+    
+    # r과 c중 하나라도 범위 밖에 있다면 넓이더해주기.
+    if not ((x <= r < x+length) and (y <= c < y+length)):
+        answer += length**2
         return
-
-    recursion(N/2, x, y)
-    recursion(N/2, x, y+N/2)
-    recursion(N/2, x+N/2, y)
-    recursion(N/2, x+N/2, y+N/2)
+    
+    # 1 사분면
+    recursion(length/2, x, y)
+    # 2 사분면
+    recursion(length/2, x, y+length/2)
+    # 3 사분면
+    recursion(length/2, x+length/2, y)
+    # 4 사분면
+    recursion(length/2, x+length/2, y+length/2)
 
 
 recursion(2**N, 0, 0)
