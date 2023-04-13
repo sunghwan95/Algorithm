7100ab5ce20f0e6a3fc5332590d3117bc56d5834
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-13 14:48:20 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -0,0 +1,16 @@
+def recursion(x):
+    global n
+    blank = "_"*(4*(x-1))
+    if x == n+1:
+        print(blank+'"재귀함수가 뭔가요?"', blank+'"재귀함수는 자기 자신을 호출하는 함수라네"', sep="\n")
+        print(blank+"라고 답변하였지.")
+        return
+    print(f'{blank}"재귀함수가 뭔가요?"\n{blank}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n{blank}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n{blank}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
+    recursion(x+1)
+    print("_"*(4*(x-1))+'라고 답변하였지.')
+    return
+
+
+n = int(input())
+print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+recursion(1)
