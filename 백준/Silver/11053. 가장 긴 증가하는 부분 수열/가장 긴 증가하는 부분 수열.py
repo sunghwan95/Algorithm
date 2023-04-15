e8cb2cb995e18bae7bacd80032a22570cd76c084
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-15 17:30:56 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -2,32 +2,10 @@ import sys
 
 N = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
-answer = [nums[0]]
+dp = [1]*N
 
-# answer와 새로 들어갈 숫자를 인자로 받아서
-# 어느 인덱스에 끼워넣을지 판단하는 함수.
-def binary(answer, num):
-    start = 0
-    end = len(answer)-1
-    while start <= end:
-        mid = (start+end)//2
-        if num < answer[mid]:
-            end = mid-1
-        elif num > answer[mid]:
-            start = mid+1
-        else:
-            return mid
-    return end+1
-
-
-for num in nums:
-    # 새로 들어갈 숫자가 answer의 마지막 요소보다 크다면
-    if num > answer[-1]:
-        # answers에 숫자 추가.
-        answer.append(num)
-    # 새로 들어갈 숫자가 answer의 마지막 요소보다 작거나 같다면
-    else:
-        # answer의 어느 인덱스에 들어가야 할 지 판단.
-        answer[binary(answer, num)] = num
-
-print(len(answer))
+for i in range(N):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
