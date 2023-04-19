d054fba0f6cce26d566fa2b307f6c30779eec7e5
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-20 04:30:57 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 896 ms, Memory: 229148 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,32 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+nums = list(map(int, sys.stdin.readline().split()))
+nums.sort()
+numsDict = dict()
+
+for num in nums:
+    if num not in numsDict:
+        numsDict[num] = 1
+    else:
+        numsDict[num] += 1
+
+M = int(sys.stdin.readline())
+cards = list(map(int, sys.stdin.readline().split()))
+for card in cards:
+    start = 0
+    end = N-1
+    count = 0
+    while start <= end:
+        mid = (start+end)//2
+        if card > nums[mid]:
+            start = mid+1
+        elif card < nums[mid]:
+            end = mid-1
+        else:
+            break
+    if card == nums[mid]:
+        print(numsDict[card], end=" ")
+    else:
+        print(0, end=" ")
