648e8a8e0d01c433a2834c80697dfc9845874e5f
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-23 00:18:28 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 392 ms, Memory: 65844 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+N = int(input())
+
+visited = [0]*(N+1)
+edges = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
+
+parent = [0]*(N+1)
+for i in range(N+1):
+    parent[i] = i
+
+
+def dfs(x):
+    for i in edges[x]:
+        if visited[i] == 0:
+            visited[i] = 1
+            parent[i] = x
+            dfs(i)
+
+
+dfs(1)
+for i in parent[2:]:
+    print(i)
