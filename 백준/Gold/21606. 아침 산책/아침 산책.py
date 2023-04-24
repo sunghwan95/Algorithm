da22cf5ba3da4e1045eb68140197ad52841a2682
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-04-25 06:47:54 +0900

    [Gold III] Title: 아침 산책, Time: 1780 ms, Memory: 69364 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
 N = int(input())
 string = list(map(str, input().rstrip()))
@@ -13,14 +14,15 @@ for _ in range(N-1):
     graph[a].append(b)
     graph[b].append(a)
 
-visited = [0]*(N+1)
-res = [0]*(N+1)
+visited = [0 for _ in range(N+1)]
+
+count = 0
 
 
 def dfs(x, start):
+    global count
     if in_out[x] == "1":
-        res[x] += 1
-        res[start] += 1
+        count += 2
         return
 
     for _next in graph[x]:
@@ -30,18 +32,16 @@ def dfs(x, start):
             visited[_next] = 0
 
 
-count = 0
 for i in range(1, N+1):
     if in_out[i] == "1":
         visited[i] = 1
         for _next in graph[i]:
             if in_out[_next] == "1":
                 if visited[_next] == 0:
-                    res[_next] += 1
-                    res[i] += 1
+                    count += 2
             else:
                 visited[_next] = 1
                 dfs(_next, i)
                 visited[_next] = 0
 
-print(sum(res))
+print(count)
