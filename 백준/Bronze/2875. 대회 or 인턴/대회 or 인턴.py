5b185be98dc359742be8a62d672d624f91070e54
성환 <119668846+sunghwan95@users.noreply.github.com>
2023-05-04 00:28:21 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+N, M, K=map(int, input().split())
+team = 0
+while True :
+    N-=2
+    M-=1
+    if N<0 or M<0 or (N+M)<K:
+        break
+    team+=1
+print(team)
