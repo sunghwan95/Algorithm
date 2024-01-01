7f75adc19f6a59aa755f325fda95cd577ffe502d
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-01-02 08:37:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 332 ms, Memory: 30744 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -27,14 +27,16 @@ rl.on("close", () => {
 class Graph {
   constructor(N) {
     this.N = N;
-    this.adjacencyMatrix = Array.from(Array(N + 1), () => Array(N + 1).fill(0));
+    // graph 탐색을 위한 2중 배열 생성.
+    this.matrix = Array.from(Array(N + 1), () => Array(N + 1).fill(0));
     this.visitedDFS = Array(N + 1).fill(false);
     this.visitedBFS = Array(N + 1).fill(false);
   }
 
-  addEdge(i, j) {
-    this.adjacencyMatrix[i][j] = 1;
-    this.adjacencyMatrix[j][i] = 1;
+  // 간선 연결
+  addEdge(a, b) {
+    this.matrix[a][b] = 1;
+    this.matrix[b][a] = 1;
   }
 
   dfs(V) {
@@ -42,29 +44,35 @@ class Graph {
     const result = [V];
 
     for (let i = 1; i <= this.N; i++) {
-      if (this.adjacencyMatrix[V][i] === 1 && !this.visitedDFS[i]) {
+      // 간선이 연결되어있고 연결된 노드를 방문하지 않았다면 dfs 재귀호출
+      if (this.matrix[V][i] === 1 && !this.visitedDFS[i]) {
         result.push(...this.dfs(i));
       }
     }
+
     return result;
   }
 
   bfs(V) {
-    let queue = [V];
+    // 안접 노드부터 탐색하기 위한 큐 이용.
+    const queue = [V];
     this.visitedBFS[V] = true;
     const result = [];
 
-    while (queue.length > 0) {
+    while (queue.length >= 1) {
       const node = queue.shift();
       result.push(node);
 
       for (let i = 1; i <= this.N; i++) {
-        if (this.adjacencyMatrix[node][i] === 1 && !this.visitedBFS[i]) {
+        // node와 연결된 인접한 정점을 방문하지 않았다면
+        // for문을 돌면서 인접한 정점을 순차적으로 큐에 넣고 실행
+        if (this.matrix[node][i] === 1 && !this.visitedBFS[i]) {
           queue.push(i);
           this.visitedBFS[i] = true;
         }
       }
     }
+
     return result;
   }
 }
