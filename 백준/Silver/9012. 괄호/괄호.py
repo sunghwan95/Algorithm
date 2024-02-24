3f78935b357680d7197c95d4940734f7e148f67d
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-02-24 13:42:04 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,43 +1,27 @@
 import sys
 
-T = int(sys.stdin.readline())
+input = sys.stdin.readline
 
+T = int(input())
 
+ans = []
 for _ in range(T):
-    stk = list(map(str, sys.stdin.readline()))
-    stk.pop()
+    stk = []
+    strings = input().rstrip()
 
-    if len(stk) % 2 == 1:
-        print("NO")
-    else:
-        i = 0
-        while True:
-            try:
-                if len(stk) == 0:
-                    print("YES")
-                    break
-                elif len(stk) == 1:
-                    print("NO")
-                    break
-
-                if stk[0] == ")":
-                    print("NO")
-                    break
-            except IndexError:
+    for string in strings:
+        if string == "(":
+            stk.append(string)
+        else:
+            if stk:
+                stk.pop()
+            else:
+                stk.append(string)
                 break
 
-            else:
-                try:
-                    if stk[i] != stk[i+1]:
-                        stk.pop(i)
-                        stk.pop(i)
-                        i = 0
-                    else:
-                        i += 1
-                except IndexError:
-                    if stk[0] != stk[-1]:
-                        print("YES")
-                        break
-                    else:
-                        print("NO")
-                        break
+    if len(stk) == 0:
+        ans.append("YES")
+    else:
+        ans.append("NO")
+
+print(*ans, sep="\n")
