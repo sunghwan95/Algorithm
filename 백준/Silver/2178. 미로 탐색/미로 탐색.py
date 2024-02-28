84103f5b73bd838bba3d9b57496c298a909bdfc8
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-02-28 19:44:44 +0900

    [Silver I] Title: 미로 탐색, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,35 +5,30 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 graph = []
-visited = [[0 for _ in range(M)] for _ in range(N)]
 
 for _ in range(N):
     _list = list(map(int, input().rstrip()))
     graph.append(_list)
 
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-
 def bfs():
+    queue = []
+    queue.append((0, 0))
+
     while queue:
-        (x, y) = queue.pop(0)
+        x, y = queue.pop(0)
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+
             if 0 <= nx < N and 0 <= ny < M:
-                if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
+                if graph[nx][ny] == 1:
                     queue.append((nx, ny))
                     graph[nx][ny] += graph[x][y]
 
+    print(graph[-1][-1])
 
-queue = []
-for x in range(N):
-    for y in range(M):
-        if graph[x][y] == 1 and visited[x][y] == 0:
-            visited[x][y] = 1
-            queue.append((x, y))
-            bfs()
 
-print(graph[-1][-1])
+bfs()
