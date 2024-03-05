0630765b61c3b7b22bddbbde4293cefa1c90e9f2
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-06 00:12:41 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-arr = []
+
+graph = []
+for i in range(1, N + 1):
+    graph.append(i)
+visited = [0 for _ in range(N)]
+res = []
 
 
-def dfs(x):
+def dfs(a):
+    visited[a] = 1
 
-    if len(arr) == M:
-        print(" ".join(map(str, arr)))
+    if len(res) == M:
+        print(*res)
         return
 
-    for i in range(x, N+1):
-        if i not in arr:
-            arr.append(i)
-            dfs(i+1)
-            arr.pop()
+    for i in range(a, N):
+        if visited[i] == 0:
+            res.append(graph[i])
+            dfs(i)
+            res.pop()
+            visited[i] = 0
 
 
-dfs(1)
+for i in range(N):
+    res.append(graph[i])
+    dfs(i)
+    res.pop()
