3dbfdeb5bb2d2566426e78193eb375e94f0e9b5c
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-08 03:11:23 +0900

    [Silver I] Title: 스타트와 링크, Time: 7268 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,38 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+graph = []
+for _ in range(N):
+    _list = list(map(int, input().split()))
+    graph.append(_list)
+visited = [0 for _ in range(N)]
+res = sys.maxsize
+
+
+def dfs(depth, index):
+    global res
+    if depth == N // 2:
+        startTeam = 0
+        linkTeam = 0
+
+        for x in range(N):
+            for y in range(N):
+                if visited[x] == 1 and visited[y] == 1:
+                    startTeam += graph[x][y]
+                elif visited[x] == 0 and visited[y] == 0:
+                    linkTeam += graph[x][y]
+
+        res = min(res, abs(startTeam - linkTeam))
+        return
+
+    for i in range(index, N):
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(depth + 1, i)
+            visited[i] = 0
+
+
+dfs(0, 0)
+print(res)
