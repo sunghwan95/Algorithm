4ce6f9ecb3ecb50e7d0faa5fac066beef4f31473
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-08 05:22:33 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -9,44 +9,39 @@ maxValue = -1e9 + 1
 minvalue = 1e9 + 1
 
 
-def calculateAccordingToIndex(x, indexOfSymbols, num):
-    if indexOfSymbols == 0:
-        num += numbers[x + 1]
-    elif indexOfSymbols == 1:
-        num -= numbers[x + 1]
-    elif indexOfSymbols == 2:
-        num *= numbers[x + 1]
-    else:
-        if num < 0 and numbers[x + 1] > 0:
-            num = (abs(num) // numbers[x + 1]) * -1
-        else:
-            num = num // numbers[x + 1]
-
-    return num
-
-
-def dfs(indexOfNumbers, indexOfSymbols, operand):
+def dfs(x, indexOfSymbols, ans):
     global maxValue, minvalue
 
-    res = calculateAccordingToIndex(indexOfNumbers, indexOfSymbols, operand)
+    if indexOfSymbols == 0:
+        ans += numbers[x + 1]
+    elif indexOfSymbols == 1:
+        ans -= numbers[x + 1]
+    elif indexOfSymbols == 2:
+        ans *= numbers[x + 1]
+    else:
+        if ans < 0 and numbers[x + 1] > 0:
+            ans = (abs(ans) // numbers[x + 1]) * -1
+        else:
+            ans = ans // numbers[x + 1]
 
     if sum(operationSymbols) == 0:
-        maxValue = max(maxValue, res)
-        minvalue = min(minvalue, res)
+        maxValue = max(maxValue, ans)
+        minvalue = min(minvalue, ans)
         return
 
     for i in range(4):
         if operationSymbols[i] > 0:
             operationSymbols[i] -= 1
-            dfs(indexOfNumbers + 1, i, res)
+            dfs(x + 1, i, ans)
             operationSymbols[i] += 1
 
 
-initNum = numbers[0]
 for i in range(4):
+    ans = numbers[0]
     if operationSymbols[i] > 0:
         operationSymbols[i] -= 1
-        dfs(0, i, initNum)
+        dfs(0, i, ans)
         operationSymbols[i] += 1
 
-print(maxValue, minvalue, sep="\n")
+print(maxValue)
+print(minvalue)
