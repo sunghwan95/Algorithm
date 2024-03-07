8b92c6b6c5a20a2380febd096b95bd297241f4a0
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-08 04:47:07 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,44 +1,47 @@
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
-nums = list(map(int, input().split()))
-operations = list(map(int, input().split()))
-max_num = -1e9
-min_num = 1e9
-answer = nums[0]
+numbers = list(map(int, input().split()))
+operationSymbols = list(map(int, input().split()))
+maxValue = -1e9 + 1
+minvalue = 1e9 + 1
 
 
-def dfs(x):
-    global answer
-    global max_num, min_num
+def dfs(x, indexOfSymbols, ans):
+    global maxValue, minvalue
 
-    if x == N:
-        max_num = max(max_num, answer)
-        min_num = min(min_num, answer)
+    if indexOfSymbols == 0:
+        ans += numbers[x + 1]
+    elif indexOfSymbols == 1:
+        ans -= numbers[x + 1]
+    elif indexOfSymbols == 2:
+        ans *= numbers[x + 1]
+    else:
+        if ans < 0 and numbers[x + 1] > 0:
+            ans = (abs(ans) // numbers[x + 1]) * -1
+        else:
+            ans = ans // numbers[x + 1]
+
+    if sum(operationSymbols) == 0:
+        maxValue = max(maxValue, ans)
+        minvalue = min(minvalue, ans)
         return
 
     for i in range(4):
-        tmp = answer
-        if operations[i] > 0:
-            if i == 0:
-                answer += nums[x]
-            elif i == 1:
-                answer -= nums[x]
-            elif i == 2:
-                answer *= nums[x]
-            else:
-                if answer >= 0:
-                    answer = answer//nums[x]
-                else:
-                    answer = (-answer//nums[x])*-1
-
-            operations[i] -= 1
-            dfs(x+1)
-            answer = tmp
-            operations[i] += 1
+        if operationSymbols[i] > 0:
+            operationSymbols[i] -= 1
+            dfs(x + 1, i, ans)
+            operationSymbols[i] += 1
 
 
-dfs(1)
-print(max_num)
-print(min_num)
+for i in range(4):
+    ans = numbers[0]
+    if operationSymbols[i] > 0:
+        operationSymbols[i] -= 1
+        dfs(0, i, ans)
+        operationSymbols[i] += 1
+
+print(maxValue)
+print(minvalue)
