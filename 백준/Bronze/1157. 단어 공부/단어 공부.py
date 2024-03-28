55fb368b050d094be8df237a9da2e21235b615ae
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-28 12:50:01 +0900

    [Bronze I] Title: 단어 공부, Time: 108 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,20 +1,41 @@
+# # 1
+# import sys
+# from collections import Counter
+
+# input = sys.stdin.readline
+
+# word = str(input().rstrip())
+# upperWord = word.upper()
+
+# eachCharCnt = Counter(upperWord)
+
+# mostCommonChar = []
+# maxCnt = max(eachCharCnt.values())
+# for char, count in eachCharCnt.items():
+#     if count == maxCnt:
+#         mostCommonChar.append(char)
+
+# if len(mostCommonChar) >= 2:
+#     print("?")
+# else:
+#     print(mostCommonChar[0])
+
 import sys
-from collections import Counter
 
 input = sys.stdin.readline
 
 word = str(input().rstrip())
 upperWord = word.upper()
 
-eachCharCnt = Counter(upperWord)
+wordCnt = []
+setWord = set(upperWord)
+for i in setWord:
+    a = upperWord.count(i)
+    wordCnt.append(a)
 
-mostCommonChar = []
-maxCnt = max(eachCharCnt.values())
-for char, count in eachCharCnt.items():
-    if count == maxCnt:
-        mostCommonChar.append(char)
-
-if len(mostCommonChar) >= 2:
+if wordCnt.count(max(wordCnt)) >= 2:
     print("?")
 else:
-    print(mostCommonChar[0])
+    for j in setWord:
+        if upperWord.count(j) == max(wordCnt):
+            print(j)
