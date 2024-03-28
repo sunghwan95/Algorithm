c689ac2a46e5ec7140b7c7db05be5494bbf7c0d2
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-28 12:28:52 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,10 +1,14 @@
 A, B = input().split()
+
 arrA = list(A)
 arrB = list(B)
+
 arrA.reverse()
 arrB.reverse()
+
 strA = "".join(arrA)
 strB = "".join(arrB)
+
 if int(strA) > int(strB):
     print(strA)
 else:
