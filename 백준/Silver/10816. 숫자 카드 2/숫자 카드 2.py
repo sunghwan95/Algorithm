db50962481fce865296a3e67da493976438cc7b6
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-29 07:38:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 812 ms, Memory: 118792 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,31 +1,17 @@
 import sys
+from collections import Counter
 
-N = int(sys.stdin.readline())
+input = sys.stdin.readline
 
-nums = list(map(int, sys.stdin.readline().split()))
-nums.sort()
-numsDict = dict()
+N = int(input())
+ownedCards = list(map(int, input().split()))
 
-for num in nums:
-    if num not in numsDict:
-        numsDict[num] = 1
-    else:
-        numsDict[num] += 1
+M = int(input())
+findCards = list(map(int, input().split()))
 
-M = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
-for card in cards:
-    start = 0
-    end = N-1
-    while start <= end:
-        mid = (start+end)//2
-        if card > nums[mid]:
-            start = mid+1
-        elif card < nums[mid]:
-            end = mid-1
-        else:
-            break
-    if card == nums[mid]:
-        print(numsDict[card], end=" ")
-    else:
-        print(0, end=" ")
+_ownedCards = Counter(ownedCards)
+for findCard in findCards:
+    ans = _ownedCards.get(findCard)
+    if not ans:
+        ans = 0
+    print(ans, end=" ")
