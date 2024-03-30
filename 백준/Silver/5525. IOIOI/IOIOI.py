c1a7bf9749b03f099bda358425129279885ebf2d
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-30 17:07:19 +0900

    [Silver I] Title: IOIOI, Time: 332 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -7,23 +7,18 @@ M = int(input())
 S = str(input().rstrip())
 
 
-def IO(N):
-    return "IO" * N + "I"
-
-
 ans = 0
 
 i = 0
+cnt = 0
 while i < M:
-    if S[i] == "O":
-        i += 1
-        continue
-    else:
-        if S[i : (2 * N + 1) + i] == IO(N):
-            a = S[i : (2 * N + 1) + i]
+    if S[i : i + 3] == "IOI":
+        cnt += 1
+        i += 2
+        if cnt == N:
             ans += 1
-            i += 2
-        else:
-            i += 1
-
+            cnt -= 1
+    else:
+        cnt = 0
+        i += 1
 print(ans)
