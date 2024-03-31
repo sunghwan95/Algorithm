182d68ddb4d65d3019922ab52085192d0fedb812
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-03-31 15:56:46 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -6,41 +6,41 @@ N = int(input())
 
 graph = []
 for _ in range(N):
-    a = list(map(str, input().rstrip()))
+    a = list(input().rstrip())
     graph.append(a)
 
-ans_width = 0
-for i in range(N):
-    width = 1
-    for j in range(N):
-        if graph[i][j] == "." and (j + 1 < N) and graph[i][j + 1] == ".":
-            width += 1
-        elif (j + 1 < N) and graph[i][j + 1] == "X":
-            if width >= 2:
-                ans_width += 1
-                width = 1
-                continue
-            else:
-                width = 1
-                continue
-    if width >= 2:
-        ans_width += 1
 
-ans_length = 0
-for j in range(N):
-    length = 1
+def count_ans(graph):
+    ans = 0
+    for row in graph:
+        count = 0
+        for cell in row:
+            if cell == ".":
+                count += 1
+            else:
+                if count >= 2:
+                    ans += 1
+                count = 0
+        if count >= 2:
+            ans += 1
+
+    return ans
+
+
+def transpose(graph):
+    transposed_graph = []
     for i in range(N):
-        if graph[i][j] == "." and (i + 1 < N) and graph[i + 1][j] == ".":
-            length += 1
-        elif (i + 1 < N) and graph[i + 1][j] == "X":
-            if length >= 2:
-                ans_length += 1
-                length = 1
-                continue
-            else:
-                length = 1
-                continue
-    if length >= 2:
-        ans_length += 1
+        transposed_row = []
+        for j in range(N):
+            transposed_row.append(graph[j][i])
+        transposed_graph.append(transposed_row)
 
-print(ans_width, ans_length, end=" ")
+    return transposed_graph
+
+
+ans_row = count_ans(graph)
+
+transposed_graph = transpose(graph)
+ans_col = count_ans(transposed_graph)
+
+print(ans_row, ans_col, end=" ")
