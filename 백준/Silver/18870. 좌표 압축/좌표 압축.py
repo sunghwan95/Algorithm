4a72f150e9504d6275912d7f7387efa7020f6191
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-05 16:16:46 +0900

    [Silver II] Title: 좌표 압축, Time: 1912 ms, Memory: 145828 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,5 +1,12 @@
+"""
+159260KB, 2004ms
+
+0.수를 리스트로 받은 후 , 중복을 제거하고 정렬된 리스트를 새롭게 생성
+1.해당 숫자가 정렬된 리스트에서 몇 번째 인덱스인지 해시맵을 이용하여 표시
+2.다시 원래 배열을 순회하면서 해당 수가 몇 번째 인덱스인지만 알면 그 수보다 작은 수의 개수를 알 수 있음.
+"""
+
 import sys
-from collections import defaultdict
 
 input = sys.stdin.readline
 
@@ -8,12 +15,12 @@ nums = list(map(int, input().split()))
 
 sorted_unique_nums = sorted(list(set(nums)))
 
-nums_index = defaultdict(int)
+nums_index = {}
 for i, num in enumerate(sorted_unique_nums):
     nums_index[num] = i
 
-compressed_nums = []
+ans = []
 for num in nums:
-    compressed_nums.append(nums_index[num])
+    ans.append(nums_index[num])
 
-print(*compressed_nums)
+print(*ans)
