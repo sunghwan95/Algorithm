5c7085b91cffae41777a8c0fd90c3ab9b16e7689
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-06 06:12:45 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 280 ms, Memory: 46260 KB -BaekjoonHub

---
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N, H, T = map(int, input().split())
+
+heap = []
+for _ in range(N):
+    giant_height = int(input())
+    heapq.heappush(heap, (-giant_height, giant_height))
+
+count = 0
+success = False
+while T > 0:
+    largest_height = heap[0][1]
+    if largest_height < H or largest_height == 1:
+        break
+
+    target_height = heapq.heappop(heap)[1] // 2
+    heapq.heappush(heap, (-target_height, target_height))
+    T -= 1
+    count += 1
+
+    if heap[0][1] < H:
+        success = True
+        break
+
+max_target_height = heap[0][1]
+if success or max_target_height < H:
+    print("YES")
+    print(count)
+else:
+    print("NO")
+    print(max_target_height)
