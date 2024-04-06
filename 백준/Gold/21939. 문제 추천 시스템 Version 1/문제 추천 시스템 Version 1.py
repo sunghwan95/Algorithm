4fdab38d22a865a7ac23f91b151208fc0ec8a3e7
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-06 20:46:17 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 248 ms, Memory: 64036 KB -BaekjoonHub

---
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
@@ -0,0 +1,48 @@
+import sys
+import heapq
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+N = int(input())
+hard_order, easy_order = [], []
+solved_orders = defaultdict(bool)
+
+for _ in range(N):
+    order, difficulty = map(int, input().split())
+    hard_order.append((-difficulty, -order))
+    easy_order.append((difficulty, order))
+    solved_orders[order] = False
+
+heapq.heapify(hard_order)
+heapq.heapify(easy_order)
+
+M = int(input())
+
+for _ in range(M):
+    instructions = input().split()
+
+    if instructions[0] == "add":
+        order, difficulty = int(instructions[1]), int(instructions[2])
+        heapq.heappush(hard_order, (-difficulty, -order))
+        heapq.heappush(easy_order, (difficulty, order))
+
+    elif instructions[0] == "recommend":
+        output_method = int(instructions[1])
+
+        if output_method == 1:
+            while solved_orders[-hard_order[0][1]]:
+                solved_orders[-hard_order[0][1]] = False
+                heapq.heappop(hard_order)
+
+            print(-hard_order[0][1])
+        else:
+            while solved_orders[easy_order[0][1]]:
+                solved_orders[easy_order[0][1]] = False
+                heapq.heappop(easy_order)
+
+            print(easy_order[0][1])
+
+    elif instructions[0] == "solved":
+        order = int(instructions[1])
+        solved_orders[order] = True
