b6fc4d8934b990d9d71e95650b5dc3a88ab66372
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-08 10:04:17 +0900

    [Gold V] Title: 적록색약, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -4,41 +4,21 @@ input = sys.stdin.readline
 
 N = int(input())
 graph = []
-visited = [[0 for _ in range(N)] for _ in range(N)]
-abnormal_visited = [[0 for _ in range(N)] for _ in range(N)]
 for _ in range(N):
     _list = list(map(str, input().rstrip()))
     graph.append(_list)
 
+visited = [[0 for _ in range(N)] for _ in range(N)]
+color_weakness_visited = [[0 for _ in range(N)] for _ in range(N)]
 
-def bfs(a, b):
-    global color
+
+def bfs(a, b, is_color_weakness, visited_arr):
     queue = []
     queue.append((a, b))
     color = graph[a][b]
 
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    while queue:
-        x, y = queue.pop(0)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < N and 0 <= ny < N:
-                if visited[nx][ny] == 0 and graph[nx][ny] == color:
-                    visited[nx][ny] = 1
-                    queue.append((nx, ny))
-
-
-def abnormal_bfs(a, b):
-    global abnormal_color
-    queue = []
-    queue.append((a, b))
-    abnormal_color = graph[a][b]
-    if abnormal_color == "R":
-        abnormal_color = "G"
+    if is_color_weakness and color == "R":
+        color = "G"
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
@@ -50,11 +30,12 @@ def abnormal_bfs(a, b):
             ny = y + dy[i]
 
             if 0 <= nx < N and 0 <= ny < N:
-                if graph[nx][ny] == "R":
-                    graph[nx][ny] = "G"
+                next_color = graph[nx][ny]
+                if is_color_weakness and next_color == "R":
+                    next_color = "G"
 
-                if abnormal_visited[nx][ny] == 0 and graph[nx][ny] == abnormal_color:
-                    abnormal_visited[nx][ny] = 1
+                if visited_arr[nx][ny] == 0 and next_color == color:
+                    visited_arr[nx][ny] = 1
                     queue.append((nx, ny))
 
 
@@ -63,16 +44,15 @@ for x in range(N):
     for y in range(N):
         if visited[x][y] == 0:
             visited[x][y] = 1
-            bfs(x, y)
+            bfs(x, y, False, visited)
             count += 1
 
 _count = 0
 for x in range(N):
     for y in range(N):
-        if abnormal_visited[x][y] == 0:
-            abnormal_visited[x][y] = 1
-            abnormal_bfs(x, y)
+        if color_weakness_visited[x][y] == 0:
+            color_weakness_visited[x][y] = 1
+            bfs(x, y, True, color_weakness_visited)
             _count += 1
 
-print(count, end=" ")
-print(_count)
+print(count, _count)
