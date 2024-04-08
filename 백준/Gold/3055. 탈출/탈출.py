5b05ef781593622eda6aa237cafaa1b743db8ce1
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-08 18:24:40 +0900

    [Gold IV] Title: 탈출, Time: 64 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -1,51 +1,58 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
 R, C = map(int, input().split())
 graph = []
-for _ in range(R):
-    a = list(map(str, input().rstrip()))
-    graph.append(a)
+w_queue = deque()
+for i in range(R):
+    row = list(input().rstrip())
+    graph.append(row)
+    for j, cell in enumerate(row):
+        if cell == "S":
+            hedgehog = (i, j)
+        elif cell == "*":
+            w_queue.append((i, j))
 
-visited = [[-1]*C for _ in range(R)]
-
-queue = deque()
-for i in range(len(graph)):
-    for j in range(len(graph[i])):
-        if graph[i][j] == "*":
-            queue.appendleft((i, j))
-        elif graph[i][j] == "S":
-            queue.append((i, j))
-            visited[i][j] = 0
-
+visited = [[0] * C for _ in range(R)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def bfs():
+    h_queue = deque([hedgehog])
 
-    while queue:
-        x, y = queue.popleft()
+    while h_queue:
+        for _ in range(len(w_queue)):
+            wx, wy = w_queue.popleft()
 
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+            for i in range(4):
+                nwx = wx + dx[i]
+                nwy = wy + dy[i]
 
-            if nx < 0 or nx >= R or ny < 0 or ny >= C:
-                continue
-            if visited[nx][ny] != -1:
-                continue
-            if graph[nx][ny] == "*" or graph[nx][ny] == "X":
-                continue
-            if graph[nx][ny] == "D" and graph[x][y] == "*":
-                continue
-            if graph[nx][ny] == "D" and graph[x][y] == "S":
-                return visited[x][y] + 1
+                if 0 <= nwx < R and 0 <= nwy < C:
+                    if graph[nwx][nwy] == ".":
+                        graph[nwx][nwy] = "*"
+                        w_queue.append((nwx, nwy))
 
-            queue.append((nx, ny))
-            visited[nx][ny] = visited[x][y]+1
-            graph[nx][ny] = graph[x][y]
+        # 물이 먼저 차오른 다음에 고슴도치 이동
+        for _ in range(len(h_queue)):
+            hx, hy = h_queue.popleft()
+
+            if graph[hx][hy] == "D":
+                return visited[hx][hy]
+
+            for j in range(4):
+                nhx = hx + dx[j]
+                nhy = hy + dy[j]
+                if 0 <= nhx < R and 0 <= nhy < C:
+                    if graph[nhx][nhy] == "." and visited[nhx][nhy] == 0:
+                        visited[nhx][nhy] = visited[hx][hy] + 1
+                        h_queue.append((nhx, nhy))
+                    elif graph[nhx][nhy] == "D":
+                        return visited[hx][hy] + 1
+
     return "KAKTUS"
 
 
