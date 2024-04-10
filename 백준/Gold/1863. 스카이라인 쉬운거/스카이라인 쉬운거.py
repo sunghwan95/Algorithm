d9dd9e36c571c8a7a18fbda31189f7922585cfe2
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-10 18:40:40 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -9,18 +9,17 @@ for _ in range(n):
     skylines.append(y)
 skylines.append(0)
 
-stk = [0]
-cnt = 0
+ans = 0
 
+stk = [0]
 for y in skylines:
     height = y
     while stk[-1] > y:
         if stk[-1] != height:
             height = stk[-1]
-            cnt += 1
+            ans += 1
         stk.pop()
 
-    if stk[-1] < y:
-        stk.append(y)
+    stk.append(y)
 
-print(cnt)
+print(ans)
