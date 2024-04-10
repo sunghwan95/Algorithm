dd41ab1356ba31dd53459db85545e20ec8355bf5
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-10 18:33:15 +0900

    [Gold IV] Title: 오큰수, Time: 1048 ms, Memory: 155536 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -5,12 +5,13 @@ input = sys.stdin.readline
 A = int(input())
 nums = list(map(int, input().split()))
 
-idx_stk = []
 ans = [-1] * A
 
+idx_stk = []
 for i, num in enumerate(nums):
     while idx_stk and nums[idx_stk[-1]] < num:
         ans[idx_stk.pop()] = num
+
     idx_stk.append(i)
 
 print(*ans)
