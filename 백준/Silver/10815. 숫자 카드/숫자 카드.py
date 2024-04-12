6145fd80b710154823686beb4033ffec2c2f3a89
성환 <119668846+sunghwan95@users.noreply.github.com>
2024-04-13 06:18:49 +0900

    [Silver V] Title: 숫자 카드, Time: 1880 ms, Memory: 112876 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,29 +1,32 @@
 import sys
 
-N = int(sys.stdin.readline())
+input = sys.stdin.readline
 
-nums = list(map(int, sys.stdin.readline().split()))
+N = int(input())
+nums = list(map(int, input().split()))
 nums.sort()
 
-M = int(sys.stdin.readline())
+M = int(input())
+check_nums = list(map(int, input().split()))
 
-cards = list(map(int, sys.stdin.readline().split()))
-answer = []
-for card in cards:
+
+def binary_search(check_num):
     start = 0
-    end = N-1
+    end = N - 1
+
     while start <= end:
-        mid = (start+end)//2
-        if card > nums[mid]:
-            start = mid+1
-        elif card < nums[mid]:
-            end = mid-1
+        mid = (start + end) // 2
+
+        if check_num == nums[mid]:
+            print(1, end=" ")
+            return
+        elif check_num > nums[mid]:
+            start = mid + 1
         else:
-            answer.append(1)
-            break
+            end = mid - 1
 
-    if nums[mid] != card:
-        answer.append(0)
+    print(0, end=" ")
 
 
-print(" ".join(map(str, answer)))
+for check_num in check_nums:
+    binary_search(check_num)
