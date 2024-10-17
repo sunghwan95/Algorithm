fe3293baa103f5f6ba372ef89357af9c7635bd43
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-10-18 00:13:31 +0900

    [Silver II] Title: 스택 수열, Time: 148 ms, Memory: 41252 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -3,25 +3,29 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-nums = [int(input()) for _ in range(n)]
 
-ans = []
+nums = []
+for _ in range(n):
+    a = int(input())
+    nums.append(a)
+
 stk = []
+ans = []
 
-now_num = 1
+now = 1
 for num in nums:
     if stk and stk[-1] == num:
-        ans.append("-")
         stk.pop()
+        ans.append("-")
         continue
 
-    for i in range(now_num, n + 1):
+    for i in range(now, n + 1):
         stk.append(i)
         ans.append("+")
         if i == num:
             stk.pop()
             ans.append("-")
-            now_num = i + 1
+            now = i + 1
             break
 
 if stk:
