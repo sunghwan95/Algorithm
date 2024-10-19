da96e60fcf02daa210e8b51ead570a57d6acd5e3
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-10-19 16:01:58 +0900

    [Silver III] Title: 프린터 큐, Time: 56 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -7,18 +7,22 @@ T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    docs = list(map(int, input().split()))
+    docs = deque(list(map(int, input().split())))
 
-    queue = deque([(idx, priority) for idx, priority in enumerate(docs)])
+    q = deque([(idx, priority) for idx, priority in enumerate(docs)])
 
-    ans = 0
-    while queue:
-        current_doc = queue.popleft()
+    want_print = q[M]
 
-        if any(current_doc[1] < elem[1] for elem in queue):
-            queue.append(current_doc)
+    cnt = 0
+    while q:
+        cur_doc = q[0]
+        if any(cur_doc[1] < elem[1] for elem in q):
+            q.append(q.popleft())
         else:
-            ans += 1
-            if current_doc[0] == M:
-                print(ans)
+            if cur_doc[0] != want_print[0]:
+                q.popleft()
+                cnt += 1
+            else:
+                cnt += 1
+                print(cnt)
                 break
