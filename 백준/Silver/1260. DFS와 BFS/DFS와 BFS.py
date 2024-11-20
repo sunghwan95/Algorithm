fd4e7df5f597533db7844d5f4ad8a5d70bbbe9bd
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-11-20 17:03:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 152 ms, Memory: 39628 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,42 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 N, M, V = map(int, input().split())
 
-graph = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-visitedDFS = [0 for _ in range(N + 1)]
-visitedBFS = [0 for _ in range(N + 1)]
-
+graph = [[0] * (N + 1) for _ in range(N + 1)]
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a][b] = 1
     graph[b][a] = 1
 
+visited_DFS = [0] * (N + 1)
+visited_BFS = [0] * (N + 1)
 
-def dfs(v):
-    visitedDFS[v] = 1
-    print(v, end=" ")
+
+def dfs(start):
+    visited_DFS[start] = 1
+    print(start, end=" ")
 
     for i in range(1, N + 1):
-        if graph[v][i] == 1 and visitedDFS[i] == 0:
+        if graph[start][i] == 1 and visited_DFS[i] == 0:
             dfs(i)
 
 
-def bfs(v):
-    queue = []
-    queue.append(v)
-    visitedBFS[v] = 1
+def bfs(start):
+    queue = deque([])
+    queue.append(start)
 
     while queue:
-        visitedNode = queue.pop(0)
+        _start = queue.popleft()
+        visited_BFS[_start] = 1
+        print(_start, end=" ")
 
-        print(visitedNode, end=" ")
-        for visitingNode in range(1, N + 1):
-            if graph[visitedNode][visitingNode] == 1 and visitedBFS[visitingNode] == 0:
-                visitedBFS[visitingNode] = 1
-                queue.append(visitingNode)
+        for i in range(1, N + 1):
+            if graph[_start][i] == 1 and visited_BFS[i] == 0:
+                visited_BFS[i] = 1
+                queue.append(i)
 
 
 dfs(V)
