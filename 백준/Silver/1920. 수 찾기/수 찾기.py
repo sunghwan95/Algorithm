16b9e785010a3ff2e5de4df2ccea4d63d38bb62a
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-11-20 18:02:44 +0900

    [Silver IV] Title: 수 찾기, Time: 400 ms, Memory: 47416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -3,33 +3,33 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-nums = list(map(int, input().split()))
+given = list(map(int, input().split()))
 
 M = int(input())
-check_nums = list(map(int, input().split()))
+check = list(map(int, input().split()))
 
-nums.sort()
+given.sort()
 
 
-def binary_search(target):
+def bst(target):
     start = 0
     end = N - 1
 
     while start <= end:
         mid = (start + end) // 2
 
-        if nums[mid] == target:
+        if target == given[mid]:
             return True
-        elif nums[mid] > target:
+        elif target < given[mid]:
             end = mid - 1
-        else:
+        elif target > given[mid]:
             start = mid + 1
 
     return False
 
 
-for check_num in check_nums:
-    if binary_search(check_num):
+for i in check:
+    if bst(i):
         print(1)
     else:
         print(0)
