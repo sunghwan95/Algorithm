ae9e0a15128254cf19ecc32b4025e03b09f3b9ea
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-12-06 12:23:51 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -4,41 +4,49 @@ input = sys.stdin.readline
 
 T = int(input())
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-def bfs():
+def bfs(x, y):
+    queue = []
+    queue.append((x, y))
+
     while queue:
-        (x, y) = queue.pop(0)
+        _x, _y = queue.pop(0)
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N:
-                if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    queue.append((nx, ny))
+            nx = _x + dx[i]
+            ny = _y + dy[i]
+
+            if (
+                0 <= nx < N
+                and 0 <= ny < M
+                and graph[nx][ny] == 1
+                and visited[nx][ny] == 0
+            ):
+                visited[nx][ny] = 1
+                queue.append((nx, ny))
 
 
 for _ in range(T):
     M, N, K = map(int, input().split())
 
-    graph = [[0 for _ in range(N)] for _ in range(M)]
-    visited = [[0 for _ in range(N)] for _ in range(M)]
+    graph = [[0] * M for _ in range(N)]
 
     for _ in range(K):
         a, b = map(int, input().split())
-        graph[a][b] = 1
+        graph[b][a] = 1
 
-    queue = []
-    count = 0
+    visited = [[0] * M for _ in range(N)]
 
-    for x in range(M):
-        for y in range(N):
-            if graph[x][y] == 1 and visited[x][y] == 0:
-                queue.append((x, y))
-                bfs()
-                count += 1
+    cnt = 0
 
-    print(count)
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j] == 0 and graph[i][j] == 1:
+                visited[i][j] = 1
+                bfs(i, j)
+                cnt += 1
+
+    print(cnt)
