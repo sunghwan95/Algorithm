cfcf562810fa5e58bd179ea692cb4b851ad91b1a
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-12-06 11:56:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,47 +1,54 @@
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
 
 graph = []
-visited = [[0 for _ in range(N)] for _ in range(N)]
 for _ in range(N):
-    _list = list(map(int, input().rstrip()))
-    graph.append(_list)
+    a = list(map(int, input().rstrip()))
+    graph.append(a)
+
+visited = [[0] * N for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-res = []
 def bfs(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
     queue = []
     queue.append((x, y))
-    houseCount = 1
 
+    cnt = 1
     while queue:
-        x, y = queue.pop(0)
+        _x, _y = queue.pop(0)
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = _x + dx[i]
+            ny = _y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < N:
-                if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    queue.append((nx, ny))
-                    houseCount += 1
-    res.append(houseCount)
+            if (
+                0 <= nx < N
+                and 0 <= ny < N
+                and graph[nx][ny] == 1
+                and visited[nx][ny] == 0
+            ):
+                visited[nx][ny] = 1
+                queue.append((nx, ny))
+                cnt += 1
+
+    return cnt
 
 
-count = 0
-for x in range(N):
-    for y in range(N):
-        if graph[x][y] == 1 and visited[x][y] == 0:
-            visited[x][y] = 1
-            bfs(x, y)
-            count += 1
+_cnt = 0
+nums = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            visited[i][j] = 1
+            a = bfs(i, j)
+            nums.append(a)
+            _cnt += 1
 
-res.sort()
-print(count)
-print(*res, sep="\n")
+nums.sort()
+print(_cnt, *nums, sep="\n")
