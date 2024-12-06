6b2398ff5de9e139f67263ce58972d02ebaecce3
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2024-12-06 11:12:33 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -4,30 +4,29 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-graph = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-visited = [0 for _ in range(N + 1)]
+
+graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
 
-res = []
+visited = [0] * (N + 1)
 
-
-def bfs(startingNode):
-    queue = []
-    queue.append(startingNode)
-    visited[startingNode] = 1
-
-    while queue:
-        visitedNode = queue.pop(0)
-
-        for visitingNode in range(1, N + 1):
-            if graph[visitedNode][visitingNode] == 1 and visited[visitingNode] == 0:
-                visited[visitingNode] = 1
-                queue.append(visitingNode)
-                res.append(visitingNode)
+cnt = 0
 
 
-bfs(1)
-print(len(res))
+def dfs(start):
+    global cnt
+
+    visited[start] = 1
+
+    for i in graph[start]:
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
+
+
+dfs(1)
+print(cnt)
