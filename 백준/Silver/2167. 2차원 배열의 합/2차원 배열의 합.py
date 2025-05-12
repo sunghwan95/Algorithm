18240a32495509e49935ec1cb8e0d4e8e2bb6f19
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2025-05-12 20:05:36 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 108 ms, Memory: 38948 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+prefixSum = [[0] * (M + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        prefixSum[i][j] = (
+            graph[i - 1][j - 1]
+            + prefixSum[i - 1][j]
+            + prefixSum[i][j - 1]
+            - prefixSum[i - 1][j - 1]
+        )
+
+K = int(input())
+for _ in range(K):
+    i, j, x, y = map(int, input().split())
+    ans = (
+        prefixSum[x][y]
+        - prefixSum[x][j - 1]
+        - prefixSum[i - 1][y]
+        + prefixSum[i - 1][j - 1]
+    )
+    print(ans)
