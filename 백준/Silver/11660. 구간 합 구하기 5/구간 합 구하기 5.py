3eb3efb7127a6d962fbfe38a22becc7e624876a2
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2025-05-12 19:48:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1024 ms, Memory: 106964 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,20 +1,27 @@
 import sys
+
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-graph = []
-for _ in range(N):
-    a = []+list(map(int, input().split()))
-    graph.append(a)
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
-
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        dp[i][j] = dp[i][j-1]+dp[i-1][j]-dp[i-1][j-1]+graph[i-1][j-1]
+prefixSum = [[0] * (N + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        prefixSum[i][j] = (
+            graph[i - 1][j - 1]
+            + prefixSum[i - 1][j]
+            + prefixSum[i][j - 1]
+            - prefixSum[i - 1][j - 1]
+        )
 
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    ans = dp[x2][y2]-dp[x2][y1-1]-dp[x1-1][y2]+dp[x1-1][y1-1]
+    ans = (
+        prefixSum[x2][y2]
+        - prefixSum[x2][y1 - 1]
+        - prefixSum[x1 - 1][y2]
+        + prefixSum[x1 - 1][y1 - 1]
+    )
     print(ans)
