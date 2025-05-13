8edca84d34d840a8255c715711aa52e128e780e5
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2025-05-13 10:17:44 +0900

    [D2] Title: 파리 퇴치, Time: 62 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -2,36 +2,27 @@ T = int(input())
 
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(N)]
 
-    graph = []
-    for _ in range(N):
-        a = list(map(int, input().split()))
-        graph.append(a)
-
-    prefixSum = [[0] * (N + 1) for _ in range(N + 1)]
-
-    for i in range(N):
-        for j in range(N):
-            prefixSum[i + 1][j + 1] = (
-                graph[i][j]
-                + prefixSum[i + 1][j]
-                + prefixSum[i][j + 1]
-                - prefixSum[i][j]
+    pSum = [[0] * (N + 1) for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            pSum[i][j] = (
+                graph[i - 1][j - 1]
+                + pSum[i][j - 1]
+                + pSum[i - 1][j]
+                - pSum[i - 1][j - 1]
             )
 
     ans = 0
-    for i in range(N - M + 1):
-        for j in range(N - M + 1):
-            x1, y1 = i, j
-            x2, y2 = i + M, j + M
-
+    for i in range(1, N - M + 2):
+        for j in range(1, N - M + 2):
             total = (
-                prefixSum[x2][y2]
-                - prefixSum[x1][y2]
-                - prefixSum[x2][y1]
-                + prefixSum[x1][y1]
+                pSum[i + M - 1][j + M - 1]
+                - pSum[i + M - 1][j - 1]
+                - pSum[i - 1][j + M - 1]
+                + pSum[i - 1][j - 1]
             )
-
             ans = max(ans, total)
 
     print(f"#{tc} {ans}")
