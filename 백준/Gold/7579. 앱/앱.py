61038f86c10786b3a01de9e087cf35812d53e42f
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2025-05-19 18:38:36 +0900

    [Gold III] Title: 앱, Time: 548 ms, Memory: 49880 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -4,23 +4,23 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-apps = list(map(int, input().split()))
+memories = list(map(int, input().split()))
 costs = list(map(int, input().split()))
+total_cost = sum(costs)
 
-zipped = list(zip(apps, costs))
+app_by_cost = list(zip(memories, costs))
 
-sum_cost = sum(costs)
-dp = [[0] * (sum_cost + 1) for _ in range(N + 1)]
+dp = [[0] * (total_cost + 1) for _ in range(N + 1)]
+# dp[i][j] = i번째 앱까지 고려했을 때, 총 비용 j에서의 최대 메모리
 
-
-for i, (memory, cost) in enumerate(zipped):
-    for j in range(sum_cost + 1):
+for i, (memory, cost) in enumerate(app_by_cost):
+    for j in range(total_cost + 1):
         dp[i + 1][j] = dp[i][j]
 
         if j >= cost:
-            dp[i + 1][j] = max(dp[i + 1][j], memory + dp[i][j - cost])
+            dp[i + 1][j] = max(dp[i][j], memory + dp[i][j - cost])
 
-for j in range(sum_cost + 1):
+for j in range(total_cost + 1):
     if dp[N][j] >= M:
         print(j)
         break
