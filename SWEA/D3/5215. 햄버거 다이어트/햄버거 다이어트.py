9cb53c05d8223b68bcc34513b767bad29e8ffb49
Dev.성환 <119668846+sunghwan95@users.noreply.github.com>
2025-05-19 20:56:53 +0900

    [D3] Title: 햄버거 다이어트, Time: 134 ms, Memory: 71,040 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,22 +1,18 @@
-def dfs(idx, total_grades, total_kcal):
-    global ans
-
-    if total_kcal > L:
-        return
-
-    ans = max(ans, total_grades)
-
-    for i in range(idx, len(ingredients)):
-        grade, kcal = ingredients[i]
-        dfs(i + 1, total_grades + grade, total_kcal + kcal)
-
-
 T = int(input())
 
 for tc in range(1, T + 1):
     N, L = map(int, input().split())
-    ingredients = [list(map(int, input().split())) for _ in range(N)]
 
-    ans = 0
-    dfs(0, 0, 0)
-    print(f"#{tc} {ans}")
+    arr = [tuple(map(int, input().split())) for _ in range(N)]
+
+    dp = [[0] * (L + 1) for _ in range(N + 1)]
+    # dp[i][j] = i번째 재료까지 고려했을 떄, j칼로리에서의 최대 점수
+
+    for i, (score, kcal) in enumerate(arr):
+        for j in range(L + 1):
+            dp[i + 1][j] = dp[i][j]
+
+            if kcal <= j:
+                dp[i + 1][j] = max(dp[i][j], score + dp[i][j - kcal])
+
+    print(f"#{tc} {max(dp[N])}")
